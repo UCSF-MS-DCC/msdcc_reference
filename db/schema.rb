d91dd1e0e06ecd8c0401ac70c1cca80f342888d0
urrik98 <adamrenschen@gmail.com>
2016-11-07 10:10:04 -0800

    [chore] created user-article relationship in db, [feat] enabled viewing of user email of author of entry, [chore] moved header markup into a rails partial for easier editing of main layout, [chore] minor layout adjustments to articles index.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104020357) do
+ActiveRecord::Schema.define(version: 20161107174015) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "term"
     t.text     "definition"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "links", force: :cascade do |t|
